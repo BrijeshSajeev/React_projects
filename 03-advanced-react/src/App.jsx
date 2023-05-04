1fb2770a4b9807db25bab9e75f83b01f53536221
BrijeshSajeev <brijesh00300@gmail.com>
2023-05-04 19:52:28 +0530

    custom hook example using fetch function

--- 03-advanced-react/src/App.jsx
+++ 03-advanced-react/src/App.jsx
@@ -17,11 +17,12 @@
 // import OtherInputs from "./tutorial/06-forms/starter/04-other-inputs";
 // import UncontrolledInputs from "./tutorial/06-forms/starter/05-form-data";
 // import UseRefBasics from "./tutorial/07-useRef/starter/01-useRef-basics";
-import ToggleExample from "./tutorial/08-custom-hooks/starter/01-toggle";
+// import ToggleExample from "./tutorial/08-custom-hooks/starter/01-toggle";
+import FetchData from "./tutorial/08-custom-hooks/starter/02-fetch-data";
 function App() {
   return (
     <div className="container">
-      <ToggleExample />
+      <FetchData />
     </div>
   );
 }
