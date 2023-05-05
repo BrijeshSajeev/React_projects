5583cb24592bcbaae9e65a5a40b4cf8c1e4cad8d
BrijeshSajeev <brijesh00300@gmail.com>
2023-05-05 19:24:27 +0530

    useReducer Dispatch and reducer

--- 03-advanced-react/src/tutorial/10-useReducer/starter/01-useReducer.jsx
+++ 03-advanced-react/src/tutorial/10-useReducer/starter/01-useReducer.jsx
@@ -1,15 +1,47 @@
-import React from "react";
+import React, { useReducer } from "react";
 import { data } from "../../../data";
+const defaultState = {
+  people: data,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "CLEAR_LIST":
+      return { ...state, people: [] };
+    case "RESET_LIST":
+      return { ...state, people: data };
+    case "REMOVE_ITEM":
+      const newPeople = state.people.filter(
+        (person) => person.id !== action.id
+      );
+      return { ...state, people: newPeople };
+    // console.log(newPeople);
+    // return { ...state };
+    default:
+      break;
+  }
+};
+
 const ReducerBasics = () => {
   const [people, setPeople] = React.useState(data);
-
+  const [state, dispatch] = useReducer(reducer, defaultState);
   const removeItem = (id) => {
-    let newPeople = people.filter((person) => person.id !== id);
-    setPeople(newPeople);
+    // let newPeople = people.filter((person) => person.id !== id);
+    // setPeople(newPeople);
+    dispatch({ type: "REMOVE_ITEM", id });
+  };
+
+  const clearList = () => {
+    // setPeople([]);
+    dispatch({ type: "CLEAR_LIST" });
+  };
+  const resetList = () => {
+    // setPeople(data);
+    dispatch({ type: "RESET_LIST" });
   };
   return (
     <div className="container">
-      {people.map((person) => {
+      {state.people.map((person) => {
         const { id, name } = person;
         return (
           <div key={id} className="item">
@@ -18,11 +50,11 @@ const ReducerBasics = () => {
           </div>
         );
       })}
-      {people.length !== 0 ? (
+      {state.people.length !== 0 ? (
         <button
           className="btn"
           style={{ marginTop: "2rem" }}
-          onClick={() => setPeople([])}
+          onClick={clearList}
         >
           clear
         </button>
@@ -30,7 +62,7 @@ const ReducerBasics = () => {
         <button
           className="btn"
           style={{ marginTop: "2rem" }}
-          onClick={() => setPeople(data)}
+          onClick={resetList}
         >
           reset
         </button>
