f561dc1e3ed7d50fcf6dad07bcc6544ff061e2c9
BrijeshSajeev <brijesh00300@gmail.com>
2023-05-05 21:20:39 +0530

    performance check using react dev tool profiler

---
+++ 03-advanced-react/src/tutorial/11-performance/starter/01-lower-state/Counter.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+function Counter() {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <button
+        className="btn"
+        onClick={() => setCount(count + 1)}
+        style={{ marginBottom: "1rem" }}
+      >
+        count {count}
+      </button>
+    </div>
+  );
+}
+export default Counter;
